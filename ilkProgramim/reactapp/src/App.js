b5afe9d291585289ad28ebaf4bb3974d765e650e
oguzhandegirmen <degirmenoguzhan60@gamil.com>
2021-07-08 19:00:39 +0300

    react example props

---
+++ ilkProgramim/reactapp/src/App.js
@@ -0,0 +1,18 @@
+import React, { ILKPROGRAMIM } from 'react';
+import ozı from "./ozı.js";
+import './App.css';
+
+
+class App extends ILKPROGRAMIM {
+render()
+  {
+  return (
+    <div className="App">
+       Merhaba Dünya
+    <ozı/>
+    </div>
+   );
+  }
+}
+
+export default App;
---
+++ ilkProgramim/reactapp/src/ozı.js
@@ -0,0 +1,17 @@
+import React, { ILKPROGRAMIM } from 'react'
+
+class ozı extends ILKPROGRAMIM {
+    render() {
+        return (
+            <div>
+                <h1>React ın daha basındayız</h1>
+                <form type="text">
+                    <button>Kaydet</button>
+                </form>
+            
+            </div>
+        )
+    }
+}
+
+export default ozı;
