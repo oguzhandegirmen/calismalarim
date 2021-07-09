f34d191649485a2d5a667aa98c8986fe65d36db4
oguzhandegirmen <degirmenoguzhan60@gamil.com>
2021-07-09 12:56:12 +0300

    grid-react-calismam

---
+++ react_grid_example/src/App.js
@@ -0,0 +1,13 @@
+import React, { react_grid_example  } from 'react';
+import grid from './grid';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react_grid_example/src/grid.js
@@ -0,0 +1,13 @@
+import React, { react_grid_example  } from 'react'
+
+class grid extends react_grid_example {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default grid;
